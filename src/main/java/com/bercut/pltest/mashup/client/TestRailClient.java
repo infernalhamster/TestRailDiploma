5f9f450753131623bbf2186b457251da23692c41
infernalhamster <vladonchikus@mail.ru>
2021-06-10 16:18:55 +0300

    realized notification ms teams

--- src/main/java/com/bercut/pltest/mashup/client/TestRailClient.java
+++ src/main/java/com/bercut/pltest/mashup/client/TestRailClient.java
@@ -4,7 +4,11 @@ import lombok.Data;
 import lombok.NonNull;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
